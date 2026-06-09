4f26735e7293afcb5979c5049d0b511ab5542a51
Jack Palfrey <jack.palf@gmail.com>
2026-06-09 14:30:09 +0100

    chore(test): use forks for better isolation

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		include: ["tests/**/*.spec.ts"],
+		pool: "forks",
 		coverage: {
 			provider: "v8",
 			include: ["src/**/*.ts"],
